4ef402cf147f0d681446ac416a7be111e1c78d11
Igor Paraiso <igorparaisod@gmail.com>
2023-04-08 12:38:33 -0300

    configurando retorno no console da resposta da chamada ao banco

--- src/services/AlunosServices/deletarAluno.ts
+++ src/services/AlunosServices/deletarAluno.ts
@@ -3,6 +3,7 @@ import execQuery from '../execQuery';
 async function deletarAluno(aluno) {
   try {
     const resultado = await execQuery.execQuery(`DELETE FROM alunos WHERE alunos.nome = '${aluno.nome}'`);
+    console.log(resultado);
   } catch (erro) {
     console.log(erro);
   }
--- src/services/AlunosServices/inserirAluno.ts
+++ src/services/AlunosServices/inserirAluno.ts
@@ -3,6 +3,7 @@ import execQuery from '../execQuery';
 async function inserirAluno(aluno) {
   try {
     const resultado = await execQuery.execQuery(`INSERT into alunos VALUES ('${aluno.nome}')`);
+    console.log(resultado);
   } catch (erro) {
     console.log(erro);
   }
