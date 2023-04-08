7fe121baba41f0433c85ddd5aa1157727ff4a618
Igor Paraiso <igorparaisod@gmail.com>
2023-04-08 11:18:59 -0300

    implementação da rota de erro 404

--- src/routes/StaticRoutes.ts
+++ src/routes/StaticRoutes.ts
@@ -1,8 +1,14 @@
-const { Router } = require('express');
-const BuildPath = require('../utils/BuildPath');
+import { Router } from 'express';
+import BuildPath from '../utils/BuildPath';
 
 const roteador = Router();
 
 roteador.get('/', async function (req, res) {
   res.sendFile(`${BuildPath(__dirname)}/views/index.html`);
 });
+
+roteador.get('*', async function (req, res) {
+  res.sendFile(`${BuildPath(__dirname)}/views/404.html`);
+});
+
+export default roteador;
